decad7fe45bb81ab85a29bee566cb70a204bfcd1
Matis Grinbaum <matis@MacBook-Pro-de-Matis.local>
2020-04-24 20:39:27 +0200

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const cors = require("cors");
+app.use(cors());
+
+document.addEventListener("DOMContentLoaded", () => {
+  const contactForm = document.querySelector("#contactForm");
+  contactForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    console.log("Le formulaire a été soumis");
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      subject: document.querySelector("#subject").value,
+      message: document.querySelector("#message").value,
+    };
+    const response = await axios.post(process.env.BACKEND_API_URL, data);
+    console.log(response);
+    if (response.status === 200) {
+      alert("Message envoyé !");
+    }
+  });
+});
